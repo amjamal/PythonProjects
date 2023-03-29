899e9714b1dc1fb2a73f33db12c26f15efbf7045
amjamal <123160136+amjamal@users.noreply.github.com>
2023-03-29 17:07:03 +0530

    Delete task4 directory

--- task4/registration/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('registration', views.reg, name='registration'),
-    path('login', views.log, name='login'),
-    path('logout', views.logout, name='logout')
-]
--- task4/task4app/urls.py
+++
@@ -1,8 +0,0 @@
-from . import views
-from django.urls import path
-app_name = 'task4app'
-
-urlpatterns = [
-    path('', views.ind, name='index'),
-    path('about/', views.about, name='about')
-]
